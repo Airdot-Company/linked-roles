7f91e81933f9873b27707766c37282926dcd32f8
TurtlePaw <turtlesimsyt@gmail.com>
2022-11-28 22:46:10 -0500

    Release package

--- README.md
+++ README.md
@@ -1,93 +1,28 @@
-<div align="center">
-<img src="https://raw.githubusercontent.com/turtlepaws-workshop/utilsfordiscord/main/images/utilsfordiscord.svg" width="150px"/>
+# @airdot/verifiers
+Verify node types and discord types
 
-<h3 style="font-size: 25.5px;">Utils for Discord</h2>
-
-🛠️ Handy utilities for Discord bots.
-
----
-
-</div>
-
-<details>
-    <summary>📃 Table of Contents</summary>
-
-- [Features](#features)
-- [Installation](#installation)
-- [Examples](#examples)
-  - [Pages](#pages)
-- [Roadmap](#roadmap)
-- [Changelog](#changelog)
-  - [6/24/2022](#6242022)
-  - [6/20/2022](#6202022)
-</details>
-
-## Features
-- 📦 No third-party libraries
-- 📥 Easy to install & use
-- 🛠️ Utilities that make sense
 ## Installation
-
+### Yarn (recommended)
 ```bash
-npm install utilsfordiscord
+yarn add @airdot/verifiers
 ```
-or
+### Node Package Manager (npm)
 ```bash
-yarn add utilsfordiscord
+npm install @airdot/verifiers
 ```
 
-## Examples
+## Questions, answers, and all that
+[![Discord Server](http://invidget.switchblade.xyz/Rgxv5M6sq9)](https://discord.gg/Rgxv5M6sq9)
 
-### Pages
-![Pages][pages]
-![You can also have custom buttons][pages-buttons]
+## Example Usage
+```ts
+import { Verifiers } from "@airdot/verifiers";
 
-> **Note**
-> This codeblock has been shortened.
-```js
-new Utils.Pages()
-    .setEmbeds([
-        new Discord.EmbedBuilder()
-        .setTitle("Embed 1")
-        .setDescription("This is an embed page you can put anything you want on it!"),
-        new Discord.EmbedBuilder()
-        .setTitle("Embed 2")
-        .setDescription("Pages also supports Discord.js v14!")
-    ])
-    .setComponents([
-        new Discord.ButtonBuilder()
-        .setLabel("Custom Button")
-        .setStyle(ButtonStyle.Success)
-        .setCustomId("custom-button"),
-        new Discord.ButtonBuilder()
-        .setLabel("Learn More")
-        .setStyle(ButtonStyle.Link)
-        .setURL("https://npm.im/utilsfordiscordjs")
-    ])
-    .setEventListener(i => i.reply("You clicked a custom button!"))
-    .send(i, {
-        disableCustomButtons: false
-    });
+Verifiers.HexColor("#5865f2")\
+//-> true
+
+// false is for the `strict` setting
+// to only allow protocol links
+Verifiers.Link("discord.gg", false)
+//-> true
 ```
-
-## Roadmap
-
-- 📦 In progress - Add more utilities
-
-## Changelog
-> **Note**
-> Format: Fixes -> Additions -> Updates -> Removals
-
-### 11/11/2022
-* Fix bug `data.components[1].components[BASE_TYPE_REQUIRED]: This field is required`
-* Remove all `//@ts-expect-error`
-### 6/24/2022
-* Add support for custom components
-* Add custom component events listeners
-### 6/20/2022
-* Added Pages class
-* Add tests
-* Add base stuff
-
-[pages-buttons]: https://turtlepaw.is-from.space/r/Discord_PMHu5ymEsQ.png
-[pages]: https://turtlepaw.is-from.space/r/MpiYO9YQH6.gif
--- tests/index.js
+++ tests/index.js
@@ -1,83 +1,16 @@
-const Discord = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { SlashCommandBuilder, ButtonStyle } = require('discord.js');
-const Utils = require("../dist/index");
-require("dotenv").config();
+const { Verifiers } = require("../dist/index");
+require("colors");
 
-const client = new Discord.Client({
-    intents: [
-        "Guilds"
-    ]
-});
-
-const commands = [
-    new SlashCommandBuilder()
-        .setName("test")
-        .setDescription("Run tests")
-];
-
-// Place your client and guild ids here
-const clientId = '988586195237888011';
-const guildId = '1028789308401918004'; //'842575277249921074';
-
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
-
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-
-        await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
+(() => {
+    console.log(`Running Hex Color Tests:`.blue + `
+• aaa is hex color string: ${Verifiers.HexColor("aaa")}
+• #5865f2 is hex color string: ${Verifiers.HexColor("#5865f2")}`.gray);
 })();
 
-client.on("interactionCreate", async i => {
-    if (!i.isChatInputCommand()) return;
-
-    new Utils.Pages()
-        .setEmbeds([
-            new Discord.EmbedBuilder()
-                .setTitle("Embed 1")
-                .setDescription("This is an embed page you can put anything you want on it!")
-                .setColor("Blurple"),
-            new Discord.EmbedBuilder()
-                .setTitle("Embed 2")
-                .setColor("Blurple")
-                .setDescription("Pages also supports Discord.js v14!")
-        ])
-        /*.setComponents([
-            new Discord.ButtonBuilder()
-                .setLabel("Custom Button")
-                .setStyle(ButtonStyle.Success)
-                .setCustomId("custom-button"),
-            new Discord.ButtonBuilder()
-                .setLabel("Learn More")
-                .setStyle(ButtonStyle.Link)
-                .setURL("https://npm.im/utilsfordiscordjs")
-        ])
-        .setEventListener(i => i.reply({
-            ephemeral: true,
-            embeds: [
-                new Discord.EmbedBuilder()
-                    .setTitle("Custom Button")
-                    .setColor("Blurple")
-                    .setDescription("This is a custom button. You can do anything with it! You can also use the `setEventListener` function to recieve interactions for custom components.\n\n>>> **TIP**\n*You can also use select menus!*")
-            ]
-        }))*/
-        .send(i, {
-            disableCustomButtons: false
-        });
-})
-
-client.login(process.env.TOKEN);
-
-setTimeout(() => {
-    process.exit();
-}, 60000);
+(() => {
+    console.log(`Running Link Tests:`.blue + `
+• aaa is link: ${Verifiers.Link("aaa")}
+• https://discord.gg/ is link: ${Verifiers.Link("https://discord.gg/")}
+• (unstrict) discord.gg is link: ${Verifiers.Link("discord.gg", false)}
+• (strict) discord.gg is link: ${Verifiers.Link("discord.gg")}`.gray);
+})();
